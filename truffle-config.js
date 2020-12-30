a568622675372137a946d11cae788a0d38b1810c
Claudia Richoux <c@laudiacay.cool>
2020-12-30 00:58:31 -0600

    optimiz

--- truffle-config.js
+++ truffle-config.js
@@ -28,7 +28,11 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.6.12"
+      version: "0.6.12",
+optimizer: {
+            enabled: true,
+            runs: 200
+        }
     }
   }
 };
